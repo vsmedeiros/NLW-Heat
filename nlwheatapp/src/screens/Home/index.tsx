74bd9c3fe942aaab9e617fa9a96e8ea65f6278e3
vsmedeiros <vitorsimedeiros@gmail.com>
2021-10-24 14:26:20 -0300

    Folder nlwheatapp

---
+++ nlwheatapp/src/screens/Home/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, KeyboardAvoidingView, Platform } from "react-native";
+import { styles } from "./styles";
+import { Header } from "../../components/Header";
+import { MessageList } from "../../components/MessageList";
+import { SigninBox } from "../../components/SigninBox";
+import { SendMessageForm } from "../../components/SendMessageForm";
+import { useAuth } from "../../hooks/auth";
+export function Home() {
+  const { user } = useAuth();
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      <View style={styles.container}>
+        <Header />
+        <MessageList />
+        {user ? <SendMessageForm /> : <SigninBox />}
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
---
+++ nlwheatapp/src/utils/messages.ts
@@ -0,0 +1,66 @@
+export const MESSAGES_EXAMPLE = [
+  {
+    id: '100',
+    text: 'Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥',
+    user: {
+      name: 'Dianne Russell',
+      avatar_url: 'https://randomuser.me/api/portraits/men/32.jpg'
+    }
+  },
+  {
+    id: '101',
+    text: 'Esse vai ser simplesmente fantástico! Bora aprender tudo em relação a montagem de APIs GraphQL. Sem contar com as palestras e painéis.',
+    user: {
+      name: 'Guy Hawkins',
+      avatar_url: 'https://randomuser.me/api/portraits/men/43.jpg'
+    }
+  },
+  {
+    id: '102',
+    text: 'Sem dúvida as palestras vão ser úteis para a minha carreira e para a de muitos 😍 #gorocket',
+    user: {
+      name: 'Eleanor Pena',
+      avatar_url: 'https://uifaces.co/our-content/donated/6f6p85he.jpg'
+    }
+  },
+  {
+    id: '103',
+    text: 'Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥',
+    user: {
+      name: 'Dianne Russell',
+      avatar_url: 'https://randomuser.me/api/portraits/men/91.jpg'
+    }
+  },
+  {
+    id: '104',
+    text: 'Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥',
+    user: {
+      name: 'Dianne Russell',
+      avatar_url: 'https://randomuser.me/api/portraits/men/61.jpg'
+    }
+  },
+  {
+    id: '105',
+    text: 'Esse vai ser simplesmente fantástico! Bora aprender tudo em relação a montagem de APIs GraphQL. Sem contar com as palestras e painéis.',
+    user: {
+      name: 'Guy Hawkins',
+      avatar_url: 'https://uifaces.co/our-content/donated/vIqzOHXj.jpg'
+    }
+  },
+  {
+    id: '106',
+    text: 'Sem dúvida as palestras vão ser úteis para a minha carreira e para a de muitos 😍 #gorocket',
+    user: {
+      name: 'Eleanor Pena',
+      avatar_url: 'https://uifaces.co/our-content/donated/AW-rdWlG.jpg'
+    }
+  },
+  {
+    id: '107',
+    text: 'Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥',
+    user: {
+      name: 'Dianne Russell',
+      avatar_url: 'https://uifaces.co/our-content/donated/n4Ngwvi7.jpg'
+    }
+  },
+];
